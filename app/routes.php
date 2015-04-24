edf4bd0f498ab8ee834178d6d96f1fc92178b8bc
Segundo Fdez <segun.2@gmail.com>
2015-04-24 13:57:42 +0200

    Add 404 custom page

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+
 $app->get('/', function () use ($app) {
 
 	$data['css'] = scandir('static/build/css', 1)[0];
@@ -10,3 +11,13 @@ $app->get('/', function () use ($app) {
 	$app->render('home.php', $data);
 
 });
+
+// ERROR 404
+$app->notFound(function () use ($app, $data) {
+
+	$data['css'] = scandir('static/build/css', 1)[0];
+	$data['js'] = scandir('static/build/js', 1)[0];
+
+	$app->render('404.php', $data);
+});
+
---
+++ app/views/404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Error 404 (Not Found)</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+
+	<link href="/favicon.ico" rel="shortcut icon">
+	<link href="/apple-touch-icon.png" rel="apple-touch-icon">
+	<link href="/static/build/css/<?php echo $css ?>" rel="stylesheet">
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+	<!--[if lt IE 9]>
+	<script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+    <div class="page">
+		<h1>ERROR 404</h1>
+	</div>
+
+	<script type="text/javascript" src="/static/build/js/<?php echo $js; ?>"></script>
+
+</body>
+</html>
