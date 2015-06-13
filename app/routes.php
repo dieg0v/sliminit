ead1f6f940c26d08f2c69770ce0c642ef0c165f9
Diego Vilariño <diego.vilarino@gmail.com>
2015-06-13 19:52:03 +0200

    group admin routes

--- app/routes.php
+++ app/routes.php
@@ -47,50 +47,60 @@ $app->notFound(function () use ($app, $data){
 //
 // ------------------------------------------------------------------
 
-
-$app->get('/login', function () use ($app, $data) {
-	$data['metas']['title'] = 'Admin';
-	$app->render('login.mustache', $data);
-});
-
-$app->post('/login', function () use ($app, $data) {
-
-	$login_usr = trim($app->request()->post('login_usr'));
-	$login_pass = trim($app->request()->post('login_pass'));
-
-	if($data['admin']['user']==$login_usr && $data['admin']['pass']==$login_pass){
-		$_SESSION['user'] = $login_usr;
-		header("Location: /admin");
-		exit;
-	}
-
-	header("Location: /logout");
-	exit;	
-});
-
-
-$app->get('/admin', function () use ($app, $data) {
-
-	if(!isset($_SESSION['user'])){
-		header("Location: /logout");
-		exit;
-	}
-	
-	$data['metas']['title'] = 'Admin';
-	$app->render('admin.mustache', $data);
-
-});
-
-$app->get('/logout', function (){
-
-	$_SESSION['user'] = null;
-	unset($_SESSION['user']);
-	session_destroy();
-
-	header("Location: /login");
+$app->get('/admin', function(){
+	header("Location: /admin/login");
 	exit;
-
 });
 
+$app->group('/admin', function () use ($app, $data) {
 
+	$app->get('/login', function () use ($app, $data) {
 
+		if(isset($_SESSION['user'])){
+			header("Location: /admin/dashboard");
+			exit;
+		}
+
+		$data['metas']['title'] = 'Admin';
+		$app->render('login.mustache', $data);
+	});
+
+	$app->post('/login', function () use ($app, $data) {
+
+		$login_usr = trim($app->request()->post('login_usr'));
+		$login_pass = trim($app->request()->post('login_pass'));
+
+		if($data['admin']['user']==$login_usr && $data['admin']['pass']==$login_pass){
+			$_SESSION['user'] = $login_usr;
+			header("Location: /admin/dashboard");
+			exit;
+		}
+
+		header("Location: /admin/logout");
+		exit;
+	});
+
+	$app->get('/logout', function (){
+
+		$_SESSION['user'] = null;
+		unset($_SESSION['user']);
+		session_destroy();
+
+		header("Location: /admin/login");
+		exit;
+
+	});
+
+	$app->get('/dashboard', function () use ($app, $data) {
+
+		if(!isset($_SESSION['user'])){
+			header("Location: /admin/logout");
+			exit;
+		}
+
+		$data['metas']['title'] = 'Admin';
+		$app->render('admin.mustache', $data);
+
+	});
+
+});
